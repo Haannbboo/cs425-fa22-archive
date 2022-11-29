631634d530cd5b4605fcb4dd997f7ca7a4dea565
Peiran Wang <peiranw3@illinois.edu>
2022-11-29 14:26:30 -0600

    pretrain function & basic commands (no testing)

--- src/idunno/client.py
+++ src/idunno/client.py
@@ -11,6 +11,21 @@ class IdunnoClient:
             argv = command.split()
             if len(argv) == 0:
                 continue
+            elif argv[0] == "train" and len(argv) > 1:
+                #send message to all worker directly? 
+                continue
+            elif argv[0] == "if" and len(argv) > 2:
+                #inference specific task by specific model in given batch size
+                continue
+            elif argv[0] == "state" and len(argv) > 1:
+                #show the job's state (demo C1)
+                continue
+            elif argv[0] == "result" and len(argv) > 1:
+                #show the result of given job (demo C4)
+                continue
+            elif argv[0] == "assign":
+                #show the current set of VMs assigned to each job (demo C5)
+                continue
             else:
                 print(f"[ERROR] Invalid command: {command}")
     
--- src/idunno/node.py
+++ src/idunno/node.py
@@ -1,7 +1,15 @@
 import threading
 from typing import List
+from transformers import AutoFeatureExtractor, AutoModelForImageClassification
 
 class IdunnoNode:
+    def __init__(self) -> None:
+        self.model_map = {}
+    
+    def pretrain(self, model_name):
+        extractor = AutoFeatureExtractor.from_pretrained("microsoft/" + model_name)
+        model = AutoModelForImageClassification.from_pretrained("microsoft/" + model_name)
+        self.model_map[model_name] = (extractor, model)
 
     def commander(self):
         pass
